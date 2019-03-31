7791fd7f30da47fbc123b34697e2388cc6afb45e
Julien <julien.courtial07@gmail.com>
2019-03-31 12:06:06 +0200

    updating set to watch list crunchyroll

--- background.js
+++ background.js
@@ -550,7 +550,7 @@ function set_to_watch_list_crunchyroll(item) {
     for (var i = crunchyroll_list.length - 1; i >= 0; i--) {
       let elem = crunchyroll_list[i];
       let num_ep = parseInt(elem.title.split(" - ")[1].split(" ")[1]);
-      if (titles.includes(elem.seriesTitle.toLowerCase())) {
+      if (titles.includes(elem.seriesTitle.toLowerCase()) && !elem.title.toLowerCase().includes("dub")) {
         if (item.last_ep_notify < num_ep) {
           item.last_ep_notify = num_ep;
           to_watch_list.push(elem);
